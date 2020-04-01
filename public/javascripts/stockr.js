9533585d19bc690966bb54ebc9a9f18f0365c039
tysovsky <tysovsky@gmail.com>
2020-04-01 15:10:34 -0400

    Added support for more stock

--- public/javascripts/stockr.js
+++ public/javascripts/stockr.js
@@ -37,7 +37,7 @@
         datasets: [{
             label: 'Stock price ($)',
             data: stock_data["TWTR"].price,
-            borderColor: "#29487D",
+            borderColor: "#1DA1F2",
             backgroundColor: '#FEFEFEA0',
             pointRadius: 0
         }]
@@ -52,6 +52,72 @@
                 backgroundColor: '#C0C0C8A0',
                 pointRadius: 0
             }]
+    }
+
+    var snap_data = {
+        labels: stock_data["SNAP"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["SNAP"].price,
+            borderColor: "#FFFC00",
+            backgroundColor: '#000000A0',
+            pointRadius: 0
+        }]
+    }
+
+    var pinterest_data = {
+        labels: stock_data["PINS"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["PINS"].price,
+            borderColor: "#E60023",
+            backgroundColor: '#FEFEFEA0',
+            pointRadius: 0
+        }]
+    }
+
+    var microsoft_data = {
+        labels: stock_data["MSFT.MI"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["MSFT.MI"].price,
+            borderColor: "#7FBA00",
+            backgroundColor: '#F25022A0',
+            pointRadius: 0
+        }]
+    }
+
+    var capitalone_data = {
+        labels: stock_data["COF"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["COF"].price,
+            borderColor: "#D22E1E",
+            backgroundColor: '#004879A0',
+            pointRadius: 0
+        }]
+    }
+
+    var wallmart_data = {
+        labels: stock_data["WMT"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["WMT"].price,
+            borderColor: "#004c91 ",
+            backgroundColor: '#78b9e7A0',
+            pointRadius: 0
+        }]
+    }
+
+    var toyota_data = {
+        labels: stock_data["TM"].date,
+        datasets: [{
+            label: 'Stock price ($)',
+            data: stock_data["TM"].price,
+            borderColor: "#572d2c ",
+            backgroundColor: '#f38698A0',
+            pointRadius: 0
+        }]
     }
 
     var chart_options = {
@@ -79,12 +145,24 @@
     var google_chart = $("#google_chart")
     var twitter_chart = $("#twitter_chart")
     var uber_chart = $("#uber_chart")
+    var snap_chart = $("#snap_chart")
+    var pinterest_chart = $("#pinterest_chart")
+    var microsoft_chart = $("#microsoft_chart")
+    var capitalone_chart = $("#capitalone_chart")
+    var wallmart_chart = $("#wallmart_chart")
+    var toyota_chart = $("#toyota_chart")
     
 
     var btns = [
         $("#btn_google"),
         $("#btn_twitter"),
-        $("#btn_uber")
+        $("#btn_uber"),
+        $("#btn_snap"),
+        $("#btn_pinterest"),
+        $("#btn_microsoft"),
+        $("#btn_capitalone"),
+        $("#btn_wallmart"),
+        $("#btn_toyota")
     ]
 
     for(var i = 0; i < btns.length; i++){
@@ -143,6 +221,104 @@
                         options: chart_options
                     })
                     break
+
+                case 'btn_snap':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#snap").show()
+                    $("#snap").addClass('currently_shown')
+
+                    
+
+                    new Chart(snap_chart, {
+                        type: 'line',
+                        data: snap_data,
+                        options: chart_options
+                    })
+                    break
+
+                case 'btn_pinterest':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#pinterest").show()
+                    $("#pinterest").addClass('currently_shown')
+
+                    
+
+                    new Chart(pinterest_chart, {
+                        type: 'line',
+                        data: pinterest_data,
+                        options: chart_options
+                    })
+                    break
+
+                case 'btn_microsoft':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#microsoft").show()
+                    $("#microsoft").addClass('currently_shown')
+
+                    
+
+                    new Chart(microsoft_chart, {
+                        type: 'line',
+                        data: microsoft_data,
+                        options: chart_options
+                    })
+                    break
+                
+                case 'btn_capitalone':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#capitalone").show()
+                    $("#capitalone").addClass('currently_shown')
+
+                    
+
+                    new Chart(capitalone_chart, {
+                        type: 'line',
+                        data: capitalone_data,
+                        options: chart_options
+                    })
+                    break
+
+                case 'btn_wallmart':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#wallmart").show()
+                    $("#wallmart").addClass('currently_shown')
+
+                    
+
+                    new Chart(wallmart_chart, {
+                        type: 'line',
+                        data: wallmart_data,
+                        options: chart_options
+                    })
+                    break
+
+                case 'btn_toyota':
+                    $(".currently_shown").hide()
+                    $(".currently_shown").removeClass('currently_shown')
+
+                    $("#toyota").show()
+                    $("#toyota").addClass('currently_shown')
+
+                    
+
+                    new Chart(toyota_chart, {
+                        type: 'line',
+                        data: toyota_data,
+                        options: chart_options
+                    })
+                    break
+
+                
             }
 
         });
